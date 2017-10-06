4321ca07954d7bf54721e09bc7cef9f351d6afac
李文周 <liwenzhou7@gmail.com>
2017-10-06 15:19:23 +0800

    update documentations

--- TryDRF/app01/urls.py
+++ TryDRF/app01/urls.py
@@ -5,41 +5,21 @@
 
 from django.conf.urls import url, include
 from app01 import views
-from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework.routers import DefaultRouter
-
-# book_list = views.BookViewSet.as_view({
-#     'get': 'list',
-#     'post': 'create'
-# })
-#
-# book_detail = views.BookViewSet.as_view({
-#     'get': 'retrieve',
-#     'put': 'update',
-#     'patch': 'partial_update',
-#     'delete': 'destroy'
-# })
+from rest_framework.schemas import get_schema_view
+from rest_framework.documentation import include_docs_urls
 
 
 # 创建路由器并注册我们的视图。
 router = DefaultRouter()
-router.register(r'^books/$', views.BookViewSet)
-router.register(r'^publishers/$', views.BookViewSet)
+router.register(r'books', views.BookViewSet)
+router.register(r'publishers', views.PublisherViewSet)
 
+schema_view = get_schema_view(title='Pastebin API')
 
 urlpatterns = [
-    # url(r'^$', views.api_root),
     url(r'^', include(router.urls)),
-    # url(r'^publishers/$', views.PublisherList.as_view(), name="publisher-list"),
-    # url(r'^publishers/(?P<pk>[0-9]+)/$', views.PublisherDetail.as_view(), name="publisher-detail"),
-    # url(r'^books/$', views.BookList.as_view(), name="book-list"),
-    # url(r'^books/(?P<pk>[0-9]+)/$', views.BookDetail.as_view(), name="bool-detail"),
-
-    # 使用viewset
-    # url(r'^books/$', book_list, name="book-list"),
-    # url(r'^books/(?P<pk>[0-9]+)/$', book_detail, name="bool-detail"),
-
+    url('^schema/$', schema_view),
+    url(r'docs/', include_docs_urls(title="图书管理系统")),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
-
-# urlpatterns = format_suffix_patterns(urlpatterns)
--- TryDRF/app01/views.py
+++ TryDRF/app01/views.py
@@ -1,130 +1,13 @@
 # -*- coding: utf-8 -*-
-from django.http import Http404
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+
 from app01 import models
 from app01 import serializers
-
-from rest_framework import mixins
-from rest_framework import generics
 from rest_framework import permissions
 from app01.permissions import IsOwnerOrReadOnly
-from rest_framework.decorators import api_view
-from rest_framework.reverse import reverse
 from rest_framework import viewsets
 
 # Create your views here.
 
-
-# class PublisherList(APIView):
-#     """
-#     列出所有的出版社，或者创建一个新的出版社
-#     """
-#
-#     def get(self, request, format=None):
-#         queryset = models.Publisher.objects.all()  # 查询出所有的出版社
-#
-#         s = serializers.PublisherSerializer(queryset, many=True)
-#         return Response(s.data)
-#
-#     def post(self, request, format=None):
-#         s = serializers.PublisherSerializer(data=request.data)
-#         if s.is_valid():  # 如果数据没问题
-#             s.save()
-#             return Response(s.data, status=status.HTTP_201_CREATED)
-#         return Response(s.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-# class PublisherList(mixins.ListModelMixin,
-#                     mixins.CreateModelMixin,
-#                     generics.GenericAPIView):
-#
-#     queryset = models.Publisher.objects.all()
-#     serializer_class = serializers.PublisherSerializer
-#
-#     def get(self, request, *args, **kwargs):
-#         return self.list(request, *args, **kwargs)
-#
-#     def post(self, request, *args, **kwargs):
-#         return self.create(request, *args, **kwargs)
-
-
-class PublisherList(generics.ListCreateAPIView):
-    queryset = models.Publisher.objects.all()
-    serializer_class = serializers.PublisherSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
-
-    def perform_create(self, serializer):
-        serializer.save(operator=self.request.user)
-
-#
-# class PublisherDetail(APIView):
-#     """
-#     具体的出版社，查看，修改，删除视图
-#     """
-#     def get_object(self, pk):
-#         try:
-#             return models.Publisher.objects.get(pk=pk)
-#         except models.Publisher.DoesNotExist:
-#             raise Http404
-#
-#     # 查看具体的出版社信息
-#     def get(self, request, pk, format=None):
-#         publisher = self.get_object(pk)
-#         s = serializers.PublisherSerializer(publisher)
-#         return Response(s.data)
-#
-#     # 修改出版社信息
-#     def put(self, request, pk, format=None):
-#         publisher = self.get_object(pk)
-#         s = serializers.PublisherSerializer(publisher, data=request.data)
-#         if s.is_valid():
-#             s.save()
-#             return Response(s.data)
-#         return Response(s.errors, status=status.HTTP_400_BAD_REQUEST)
-#
-#     # 删除出版社信息
-#     def delete(self, request, pk, format=None):
-#         publisher = self.get_object(pk)
-#         publisher.delete()
-#         return Response(status=status.HTTP_204_NO_CONTENT)
-
-
-# class PublisherDetail(mixins.RetrieveModelMixin,
-#                       mixins.UpdateModelMixin,
-#                       mixins.DestroyModelMixin,
-#                       generics.GenericAPIView):
-#
-#     queryset = models.Publisher.objects.all()
-#     serializer_class = serializers.PublisherSerializer
-#
-#     def get(self, request, *args, **kwargs):
-#         return self.retrieve(request, *args, **kwargs)
-#
-#     def put(self, request, *args, **kwargs):
-#         return self.update(request, *args, **kwargs)
-#
-#     def delete(self, request, *args, **kwargs):
-#         return self.destroy(request, *args, **kwargs)
-
-
-class PublisherDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = models.Publisher.objects.all()
-    serializer_class = serializers.PublisherSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
-
-
-# class BookList(generics.ListCreateAPIView):
-#     queryset = models.Book.objects.all()
-#     serializer_class = serializers.BookSerializer
-#     permission_classes = (permissions.IsAuthenticatedOrReadOnly, )
-#
-#
-# class BookDetail(generics.RetrieveUpdateDestroyAPIView):
-#     queryset = models.Book.objects.all()
-#     serializer_class = serializers.BookSerializer
-#     permission_classes = (permissions.IsAuthenticatedOrReadOnly, )
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = models.Book.objects.all()
@@ -132,15 +15,10 @@ class BookViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.IsAuthenticatedOrReadOnly, )
 
 
-# class PublisherViewSet(viewsets.ModelViewSet):
-#     queryset = models.Publisher.objects.all()
-#     serializer_class = serializers.PublisherSerializer
-#     permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
+class PublisherViewSet(viewsets.ModelViewSet):
+    queryset = models.Publisher.objects.all()
+    serializer_class = serializers.PublisherSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
 
-
-@api_view(['GET'])
-def api_root(request, format=None):
-    return Response({
-        'publishers': reverse('publisher-list', request=request, format=format),
-        # 'books': reverse('book-list', request=request, format=format)
-    })
+    def perform_create(self, serializer):
+        serializer.save(operator=self.request.user)
